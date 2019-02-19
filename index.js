708e0b3d9902ec40c2ddc88bb2b9a9cf796b10d6
dennistikhomirov <dennistikhomirov@gmail.com>
2019-02-19 01:02:30 -0800

    scrapper

--- index.js
+++ index.js
@@ -4,30 +4,79 @@ const xlsx = require('node-xlsx');
 run();
 
 async function run() {
+    let counterProducts = 0
+
+    const fileLog = './logs/log' + new Date().toUTCString() + '.log'
+    fs.ensureFileSync(fileLog)
+    fs.appendFileSync(fileLog, new Date().toUTCString() + "\n")
+
     const filter = JSON.parse(fs.readFileSync("filter.json", "utf8"))
 
     let sourceFile = filter.sourceFile
     if ( !sourceFile ) sourceFile = "./data/testParser.xlsx"
-    const workSheets = xlsx.parse(sourceFile)
-
     const file = './results/results' + new Date().toUTCString() + '.csv'
-    let data = workSheets[0].data
     fs.ensureFileSync(file)
     fs.appendFileSync(file, filter.firstRow + "\n")
 
+    const workSheets = xlsx.parse(sourceFile)
+    let data = workSheets[0].data
+    let isExclude
+
     for ( let i = 1; i < data.length; i++ ) {
+        isExclude = false
 
+        //price
+        const indexPrice = filter.firstRow.indexOf("Price")
+        let indexPriceBase = filter.firstRow.indexOf(filter.price.base)
+        if ( data[i][indexPriceBase] === undefined ) {
+            indexPriceBase = filter.firstRow.indexOf("Price opt")
+        }
+
+        if ( filter.price.multiply !== "" ) {
+            data[i][indexPrice] = (parseFloat(data[i][indexPriceBase]) * parseFloat(filter.price.multiply))
+            if ( (data[i][indexPrice] % 1) < 0.3 ) data[i][indexPrice]--
+            data[i][indexPrice] = Math.trunc(data[i][indexPrice]) + (Math.random() / 3.41) + 0.7
+            data[i][indexPrice] = await convertToString(data[i][indexPrice])
+        }
+
+        if ( data[i][indexPrice] <= data[i][filter.firstRow.indexOf("Price opt")] ) {
+            data[i][indexPrice] = await convertToString((parseFloat(data[i][indexPrice] )*1.3) * (1 / filter.price.multiply))
+        }
+        if ( data[i][indexPrice] < 1 ) isExclude = true
+
+        console.log("ID = " + data[i][0] + "  Price opt  =" + data[i][4] +
+            "  Price rec  =" + data[i][7] + "  Price =" + data[i][indexPrice])
+
+        // exclude
+        let where
+        for ( let k = 0; k < filter.exclude.length; k++ ) {
+            where = filter.firstRow.indexOf(filter.exclude[k].column)
+            if ( (where !== -1) && (data[i][where].toString() === filter.exclude[k].attr) ) {
+                isExclude = true
+            }
+        }
+
+        //replace
         for ( let j = 0; j < data[i].length; j++ ) {
-            //data[i][j] = await remove(data[i][j])
-            //console.log(data[i][j])
-
             data[i][j] = await replace(data[i][j], j)
-            if (data[i][j] !== undefined)  data[i][j]= data[i][j].toString().trim()
+            if ( data[i][j] !== undefined ) data[i][j] = data[i][j].toString().trim()
             data[i][j] = "\"" + data[i][j] + "\""
         }
-        fs.appendFileSync(file, data[i] + "\n")
-    }
 
+        if ( !isExclude ) {
+            fs.appendFileSync(file, data[i] + "\n")
+            counterProducts++
+        } else {
+            fs.appendFileSync(fileLog, i + " Failed to import: ID=" + data[i][filter.firstRow.indexOf("ID")] +
+                " Name: " + data[i][filter.firstRow.indexOf("Name")] +
+                " In stock =" + data[i][filter.firstRow.indexOf("In stock?")] +
+                " Price opt =" + data[i][filter.firstRow.indexOf("Price opt")] +
+                "\n")
+        }
+    }
+    fs.appendFileSync(fileLog, "\n" + counterProducts + " products were IMPORTED\n")
+    fs.appendFileSync(fileLog, (data.length - 1 - counterProducts) + " products were SKIPPED")
+    fs.appendFileSync(fileLog, (data.length + counterProducts) + " ALL PRODUCTS")
 }
 
 async function remove(data) {
@@ -51,5 +100,9 @@ async function replace(data, column) {
         } while ( data.includes(filter.replace[i].from) )
     }
     return data
-
 }
+
+async function convertToString(data) {
+    return data.toString().substring(0, data.toString().indexOf(".") + 2) + "0"
+}
+
--- scraper.js
+++ scraper.js
@@ -1,53 +1,138 @@
 /* Scraper KissExpo product images
-/ Must be logged in
-/
+
  */
+
+const jsdom = require("jsdom")//browser's mock since node doesn't contains any browser
+const { JSDOM } = jsdom;
+
+const fetch = require('node-fetch')
+const fs = require('fs-extra')
 const webdriver = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
 const { By, Key, until } = webdriver
 const chromedriver = require('chromedriver');
 let driver
-const category =By.className("col-sm-2 col-xs-6")
+const category = By.className("col-sm-2 col-xs-6")
+const product = By.className("product-layout col-lg-2 col-md-2 col-sm-3 col-xs-6")
 
-run()
+//test()
+async function test() {
+    let a = new Map()
+    let out = { elements: [{ key: "", values: [] }] }
+    out.elements[0].key = "0"
+    console.log(out.elements[0].key)
+}
 
-async function run(){
-    driver = await startChrome()
-    await driver.get("http://kissexpo.by/intim-tovary-optom/")
+scrapSexToys()
 
-    await waitUntilShowUp(category)
-    const categories = await driver.findElements(category)
-    console.log("categoriesNumbers: "+ categories.length)
+async function scrapSexToys() {
+    let counterImages=0
+    //const images = new Map()
+    let linksProducts = []
+    let a = []
+    /*
+        driver = await startChrome()
+        linksProducts = await collectProductLinks("http://kissexpo.by/intim-tovary-optom/")
+        console.log(linksProducts.length)
+        linksProducts =linksProducts.concat( await collectProductLinks("http://kissexpo.by/eroticheskoje-beljo-optom/"))
+        console.log(linksProducts.length)
 
-    for (let i=0;i<categories.length;i++){
-        await categories[0].click()
-        console.log("categoriesNumbers: "+ categories.length)
+
+
+        const fileProducts = './results/json/productsLinks.json'
+        fs.ensureFileSync(fileProducts)
+        fs.writeFileSync(fileProducts, JSON.stringify({ links: linksProducts }))
+        //console.log("Products were found:  " + linksProducts.length)
+
+        const obj = JSON.parse(fs.readFileSync("./results/json/productsLinks.json", "utf8"))
+        console.log("File products:  " + obj.links.length)
+        */
+
+    // collect product's image's links
+    let html, DOM, links
+    const obj = JSON.parse(fs.readFileSync("./results/json/productsLinks.json", "utf8"))
+
+    let out = { elements: [] }
+
+    for ( let i = 0; obj.links.length; i++ ) {
+        console.log(obj.links[i])
+        html = await fetch(obj.links[i]).then(res => res.text())
+        DOM = new JSDOM(html).window.document
+
+        let element = { key: "", values: [] }
+        element.key = (await DOM.getElementsByName('product_id'))[0].value
+        console.log(element.key)
+        links = (await DOM.getElementsByClassName('thumbnail'))
+        console.log("links.length" + links.length)
+        for ( let j = 0; j < links.length; j++ ) {
+            await element.values.push(links[j].href)
+            counterImages++
+            console.log(element.values[j])
+        }
+        console.log("element" + element)
+        await out.elements.push(element)
     }
+    console.log(out.elements.length + "  products were scanned")
+    console.log(counterImages + "  images were saved")
+    const fileImagesLinks = './results/json/imagesLinks.json'
+    fs.ensureFileSync(fileImagesLinks)
+    fs.writeFileSync(fileImagesLinks, JSON.stringify(out))
+
 
 }
 
+async function collectProductLinks(linkMainCategory) {
+
+    let linksSubCategories = []
+    let linksProducts = []
+    let products
+
+    await driver.get(linkMainCategory)
+    await waitUntilShowUp(category)
+    const categories = await driver.findElements(category)
+    delay(1000)
+    for ( let i = 0; i < categories.length; i++ ) {
+        linksSubCategories.push(await categories[i].findElement(By.tagName("a")).getAttribute("href"))
+    }
+    console.log("categoriesNumbers: " + linksSubCategories.length)
 
 
-function delay (time) {
+    for ( let linkSubCategory of linksSubCategories ) {
+        console.log("SUB: " + linkSubCategory)
+        await driver.get(linkSubCategory + "/?limit=1000")
+        await waitUntilShowUp(product)
+        products = await driver.findElements(product)
+        for ( let i = 0; i < products.length; i++ ) {
+            linksProducts.push(await products[i].findElement(By.tagName("a")).getAttribute("href"))
+            //console.log(linksProducts[i])
+        }
+    }
+    return linksProducts
+}
+
+
+function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time))
 }
 
-async function waitUntilShowUp (by, Twait) {
-    if (Twait === undefined) Twait = 200
+async function waitUntilShowUp(by, Twait) {
+    if ( Twait === undefined ) Twait = 200
     do {
         await delay(100)
-        if (await isElementDisplayed(by)) return await driver.findElement(by)
-    } while (Twait-- > 0)
+        if ( await isElementDisplayed(by) ) return await driver.findElement(by)
+    } while ( Twait-- > 0 )
     return false
 }
-async function isElementDisplayed (by) {
+
+async function isElementDisplayed(by) {
     try {
         return await driver.findElement(by).isDisplayed()
-    } catch (err) {
+    } catch ( err ) {
         return false
     }
 }
-async function startChrome () {
+
+async function startChrome() {
     chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
     let options = new chrome.Options();
     options.addArguments('disable-popup-blocking');
